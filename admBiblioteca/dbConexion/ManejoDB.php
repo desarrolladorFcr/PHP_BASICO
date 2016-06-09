131f6ea335a5d4b7fa62d1a10bafc8229ab654c9
felipeCR <cifuentesrfelipe@gmail.com>
2016-06-09 13:18:26 -0500

    Agregar parámetro de carácteres especiales

--- admBiblioteca/dbConexion/ManejoDB.php
+++ admBiblioteca/dbConexion/ManejoDB.php
@@ -1,25 +1,27 @@
 <?php
-class ManejoDB
-{
-	private static function proceso($sentencia){
-		try{
-			$conx = new PDO("mysql:host=localhost;dbname=biblioteca","root","adriana");
-			$conx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			$res = $conx->query($sentencia);
-			$conx= null;
-			return $res;
-		}catch (PDOException $err){
-			echo "Fallo en conexion".$err->getMessage();
-		}
-	
-	}
-	
-	protected static function crearActualizarEliminar($sentencia) {
-		self::proceso($sentencia);
-	}
-	
-	protected static function leer($sentencia) {
-		$res= self::proceso($sentencia);
-		return $res;
-	}
+
+class ManejoDB {
+
+    private static function proceso($sentencia) {
+        try {
+            $conx = new PDO("mysql:host=localhost;dbname=practica", "usuario", "contraseña", 
+                    array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''));
+            $conx->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $res = $conx->query($sentencia);
+            $conx = null;
+            return $res;
+        } catch (PDOException $err) {
+            echo "Fallo en conexion" . $err->getMessage();
+        }
+    }
+
+    protected static function crearActualizarEliminar($sentencia) {
+        self::proceso($sentencia);
+    }
+
+    protected static function leer($sentencia) {
+        $res = self::proceso($sentencia);
+        return $res;
+    }
+
 }
